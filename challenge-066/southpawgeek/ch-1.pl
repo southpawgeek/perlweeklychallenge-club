bcdb1164d0a4280b048a69bf93aa87a711bec651
Jen G <jen@southpawgeek.com>
2020-06-27 14:06:46 -0400

    empty files with challenge text

---
+++ challenge-066/southpawgeek/ch-1.pl
@@ -0,0 +1,20 @@
+
+
+=challenge1
+You are given two integers $M and $N.
+
+Write a script to divide the given two integers i.e. $M / $N without using multiplication, division and mod operator and return the floor of the result of the division.
+Example 1:
+
+Input: $M = 5, $N = 2
+Output: 2
+
+Example 2:
+
+Input: $M = -5, $N = 2
+Output: -3
+
+Example 3:
+
+Input: $M = -5, $N = -2
+Output: 2
